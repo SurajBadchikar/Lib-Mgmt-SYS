722853300864738251cc05af13d63d2870472d3a
SurajBadchikar <148080829+SurajBadchikar@users.noreply.github.com>
2023-12-12 15:37:01 +0530

    Add files via upload

---
+++ B_Store/footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Ouick Links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php"> New Arrivals</a>
+         <a href="contact.php">Contact Us</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i style="color: #051b5e ;" class="fas fa-phone"></i>+919243547478</p>
+         <p> <i style="color: #051b5e ;"  class="fas fa-phone"></i>+919011445678</p>
+         <p> <i style="color: #051b5e ;" class="fas fa-envelope"></i>OrionBooks@gmail.com</p>
+         <p> <i style="color: #051b5e ;" class="fas fa-map-marker-alt"></i>Pune,Maharastra,India- 411014 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i style="color: #051b5e ;"  class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i style="color: #051b5e ;" class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i style="color: #051b5e ;" class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i style="color: #051b5e ;" class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p style="color: #051b5e ;"  class="credit"> &copy; copyright <?php echo date('Y'); ?> by <span style="color: #051b5e ;" >Orion Books </span> </p>
+
+</section>
